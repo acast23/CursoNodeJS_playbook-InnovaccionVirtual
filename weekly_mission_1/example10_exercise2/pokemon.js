40eb5bc31209883b52f8d7443b71db10aa7a90c0
acast23 <antoniocastillob@hotmail.com>
2022-04-26 15:53:45 -0500

    CREATED example10 exercise2
    exercise 2 completed using "export default"

---
+++ weekly_mission_1/example10_exercise2/pokemon.js
@@ -0,0 +1,10 @@
+export default class pokemon {
+    constructor (name) {
+      this.name = name     
+    }
+
+    sayHello(){
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+}
+  
